840cb8af34eea85db1308e8f171f3d1d04e6681a
ASHANTENNA <147976992+ASHANTENNA@users.noreply.github.com>
2023-11-11 15:33:48 +0100

    Update InstalllerScript.sh

--- InstalllerScript.sh
+++ InstalllerScript.sh
@@ -6,3 +6,16 @@ echo "2. Install HTTP Proxy"
 echo "3. Install DNSTT, DoH and DoT"
 echo "4. Install ADMRufu MOD"
 echo "5. Exit"
+selected_option=0
+
+while [ $selected_option -lt 1 ] || [ $selected_option -gt 5 ]; do
+    echo "Select a number from 1 to 5:"
+    read input
+
+    # Check if input is a number
+    if [[ $input =~ ^[0-9]+$ ]]; then
+        selected_option=$input
+    else
+        echo "Invalid input. Please enter a valid number."
+    fi
+done
