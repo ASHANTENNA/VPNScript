fe5550b838681e379d9a29ae187d811b3dad2036
ASHANTENNA <147976992+ASHANTENNA@users.noreply.github.com>
2023-12-26 20:00:51 +0100

    Update InstalllerScript.sh

--- InstalllerScript.sh
+++ InstalllerScript.sh
@@ -316,17 +316,17 @@ EOF
         cd dns2tcp
         mkdir /var/empty
         mkdir /var/empty/dns2tcp
-        read -p "Your Nameserver : " $nameserver
-        read -p "Your key : " $key
+        read -p "Your Nameserver : " nameserver
+        read -p "Your key : " key
         file_path="/root/dns2tcp/dns2tcpdrc"
         json_content=$(cat <<-EOF
-    listen = 0.0.0.0
-    port = 53
-    user = ashtunnel
-    chroot = /var/empty/dns2tcp/
-    domain = $nameserver
-    key = $key
-    resources = ssh:127.0.0.1:22
+listen = 0.0.0.0
+port = 53
+user = ashtunnel
+chroot = /var/empty/dns2tcp/
+domain = $nameserver
+key = $key
+resources = ssh:127.0.0.1:22
 EOF
 )
         echo "$json_content" > "$file_path"
