600580d6d119891ead708f8f72a81fa79548afa1
ASHANTENNA <147976992+ASHANTENNA@users.noreply.github.com>
2025-03-14 23:49:43 +0100

    Update InstallerScript.sh

--- InstallerScript.sh
+++ InstallerScript.sh
@@ -45,12 +45,12 @@ echo "Version : 4.5"
 echo -e "$NC
 Select an option"
 echo "1. Install UDP Hysteria V1.3.5"
-echo "2. Install UDP Hysteria V2.6.1"
+echo "2. Install ASH WSS(port 8443)"
 echo "3. Install ASH HTTP Proxy"
 echo "4. Install DNSTT, DoH and DoT"
 echo "5. Install VPS AGN"
 echo "6. Install DNS2TCP"
-echo "7. Install WS(port 8080)"
+echo "7. Install ASH WS(port 8080)"
 echo "8. Install BadVPN UDPGW(port 7300)"
 echo "9. Install ASH SSL"
 echo "0. Exit"
@@ -203,161 +203,7 @@ EOF
         ;;
     2)
         echo -e "$YELLOW"
-        echo "Installing UDP Hysteria V2.6.1 ..."
-        echo -e "$NC"
-        apt -y update && apt -y upgrade
-        apt -y install wget nano net-tools openssl iptables-persistent screen lsof
-        rm -rf hy2
-        mkdir hy2
-        cd hy2
-        wget github.com/apernet/hysteria/releases/download/app/v2.6.1/hysteria-linux-amd64
-        chmod 755 hysteria-linux-amd64
-        openssl ecparam -genkey -name prime256v1 -out ca.key
-        openssl req -new -x509 -days 36500 -key ca.key -out ca.crt -subj "/CN=bing.com"
-        while true; do
-            echo -e "$YELLOW"
-            read -p "Obfs : " obfs
-            echo -e "$NC"
-            if [ ! -z "$obfs" ]; then
-            break
-            fi
-        done
-        while true; do
-            echo -e "$YELLOW"
-            read -p "Auth Str : " auth_str
-            echo -e "$NC"
-            if [ ! -z "$auth_str" ]; then
-            break
-            fi
-        done
-        while true; do
-            echo -e "$YELLOW"
-            read -p "Remote UDP Port : " remote_udp_port
-            echo -e "$NC"
-            if is_number "$remote_udp_port" && [ "$remote_udp_port" -ge 1 ] && [ "$remote_udp_port" -le 65534 ]; then
-                break
-            else
-                echo -e "$YELLOW"
-                echo "Invalid input. Please enter a valid number between 1 and 65534."
-                echo -e "$NC"
-            fi
-        done
-        file_path="/root/hy2/config.yaml"
-        json_content=$(cat <<-EOF
-listen: :$remote_udp_port
-tls:
-  cert: ca.crt
-  key: ca.key
-obfs:
-  type: salamander
-  salamander:
-    password: $obfs
-quic:
-  initStreamReceiveWindow: 16777216
-  maxStreamReceiveWindow: 16777216
-  initConnReceiveWindow: 33554432
-  maxConnReceiveWindow: 33554432
-auth:
-  type: password
-  password: $auth_str
-masquerade:
-  type: proxy
-  proxy:
-    url: https://223.5.5.5/dns-query
-    rewriteHost: true
-EOF
-)
-        echo "$json_content" > "$file_path"
-        if [ ! -e "$file_path" ]; then
-            echo -e "$YELLOW"
-            echo "Error: Unable to save the config.json file"
-            echo -e "$NC"
-            exit 1
-        fi
-
-        echo -e "$YELLOW"
-        read -p "Bind multiple UDP Ports? (y/n): " bind
-        echo -e "$NC"
-        if [ "$bind" = "y" ]; then
-            while true; do
-                echo -e "$YELLOW"
-                read -p "Binding UDP Ports : from port : " first_number
-                echo -e "$NC"
-                if is_number "$first_number" && [ "$first_number" -ge 1 ] && [ "$first_number" -le 65534 ]; then
-                    break
-                else
-                    echo -e "$YELLOW"
-                    echo "Invalid input. Please enter a valid number between 1 and 65534."
-                    echo -e "$NC"
-                fi
-            done
-            while true; do
-                echo -e "$YELLOW"
-                read -p "Binding UDP Ports : from port : $first_number to port : " second_number
-                echo -e "$NC"
-                if is_number "$second_number" && [ "$second_number" -gt "$first_number" ] && [ "$second_number" -lt 65536 ]; then
-                    break
-                else
-                    echo -e "$YELLOW"
-                    echo "Invalid input. Please enter a valid number greater than $first_number and less than 65536."
-                    echo -e "$NC"
-                fi
-            done
-            #Remove old rules
-            iptables -t nat -L --line-numbers | awk -v var="$first_number:$second_number" '$0 ~ var {print $1}' | tac | xargs -r -I {} iptables -t nat -D PREROUTING {}
-            ip6tables -t nat -L --line-numbers | awk -v var="$first_number:$second_number" '$0 ~ var {print $1}' | tac | xargs -r -I {} ip6tables -t nat -D PREROUTING {}
-            iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport "$first_number":"$second_number" -j DNAT --to-destination :$remote_udp_port
-            ip6tables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport "$first_number":"$second_number" -j DNAT --to-destination :$remote_udp_port
-            iptables-save > /etc/iptables/rules.v4
-            ip6tables-save > /etc/iptables/rules.v6
-        fi
-        
-        warpv6=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-        warpv4=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-        [[ $warpv4 =~ on|plus || $warpv6 =~ on|plus ]]
-        wg-quick down wgcf >/dev/null 2>&1
-        systemctl stop warp-go >/dev/null 2>&1
-        systemctl start warp-go >/dev/null 2>&1
-        wg-quick up wgcf >/dev/null 2>&1
-        
-        sysctl net.ipv4.conf.all.rp_filter=0
-        sysctl net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0 
-        echo "net.ipv4.ip_forward = 1
-        net.ipv4.conf.all.rp_filter=0
-        net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0" > /etc/sysctl.conf
-        sysctl -p
-
-        echo -e "$YELLOW"
-        read -p "Run in background or foreground service ? (b/f): " bind
-        echo -e "$NC"
-        if [ "$bind" = "b" ]; then
-            screen -dmS hy2 ./hysteria-linux-amd64 server
-        else
-            json_content=$(cat <<-EOF
-[Unit]
-Description=Daemonize UDP Hysteria V2 Tunnel Server
-Wants=network.target
-After=network.target
-[Service]
-ExecStart=/root/hy2/hysteria-linux-amd64 server -c /root/hy2/config.yaml
-Restart=always
-RestartSec=3
-[Install]
-WantedBy=multi-user.target
-EOF
-)
-            echo "$json_content" > /etc/systemd/system/hy2.service
-            systemctl start hy2
-            systemctl enable hy2
-        fi
-
-        lsof -i :"$remote_udp_port"
-        echo -e "$YELLOW"
-        echo "UDP Hysteria V2.3.0 installed successfully, please check the logs above"
-        echo "IP Address :"
-        curl ipv4.icanhazip.com
-        echo "Obfs : '"$obfs"'"
-        echo "auth str : '"$auth_str"'"
+        echo "ASH WSS Coming soon ..."
         echo -e "$NC"
         exit 1
         ;;
@@ -581,7 +427,7 @@ EOF
         ;;
     7)
         echo -e "$YELLOW"
-        echo "Installing WS..."
+        echo "Installing ASH WS..."
         echo -e "$NC"
         apt -y update && apt -y upgrade
         apt -y install iptables-persistent wget lsof
