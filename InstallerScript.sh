a5cc1023bf6c2a7e5c70f0867c6ec03dfa3e9d49
ASHANTENNA <147976992+ASHANTENNA@users.noreply.github.com>
2025-09-13 23:57:19 +0100

    Update InstallerScript.sh

--- InstallerScript.sh
+++ InstallerScript.sh
@@ -388,11 +388,24 @@ EOF
         iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
         iptables-save > /etc/iptables/rules.v4
 
+        while true; do
+            echo -e "$YELLOW"
+            read -p "Target TCP Port : " target_port
+            echo -e "$NC"
+            if is_number "$target_port" && [ "$target_port" -ge 1 ] && [ "$target_port" -le 65535 ]; then
+                break
+            else
+                echo -e "$YELLOW"
+                echo "Invalid input. Please enter a valid number between 1 and 65535."
+                echo -e "$NC"
+            fi
+        done
+
         echo -e "$YELLOW"
         read -p "Run in background or foreground service ? (b/f): " bind
         echo -e "$NC"
         if [ "$bind" = "b" ]; then
-            screen -dmS slowdns ./dnstt-server -udp :5300 -privkey-file server.key $ns 127.0.0.1:22
+            screen -dmS slowdns ./dnstt-server -udp :5300 -privkey-file server.key $ns 127.0.0.1:$target_port
         else
             json_content=$(cat <<-EOF
 [Unit]
@@ -400,7 +413,7 @@ Description=Daemonize DNSTT Tunnel Server
 Wants=network.target
 After=network.target
 [Service]
-ExecStart=/root/dnstt/dnstt-server -udp :5300 -privkey-file /root/dnstt/server.key $ns 127.0.0.1:22
+ExecStart=/root/dnstt/dnstt-server -udp :5300 -privkey-file /root/dnstt/server.key $ns 127.0.0.1:$target_port
 Restart=always
 RestartSec=3
 [Install]
